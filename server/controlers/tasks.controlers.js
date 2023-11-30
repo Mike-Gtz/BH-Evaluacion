26d2c49600926ed57e82b7d3e784678e3fee1e50
MIGUEL GUTIERREZ MEDINA <2019371080@uteq.edu.mx>
2023-11-30 12:38:15 -0600

    Funcionalidad del BackEnd

--- server/controlers/tasks.controlers.js
+++ server/controlers/tasks.controlers.js
@@ -1,23 +1,48 @@
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-    res.send('obteniendo tareas')
+export const getTasks = async(req, res) => {
+
+    const [result] = await pool.query('SELECT * FROM discos');
+    res.json(result);
 }
 
-export const getTask = (req, res) => {
-    res.send('obteniendo tarea')
+export const getTask = async(req, res) => {
+    const [result] = await pool.query('SELECT * FROM discos WHERE id = ?', [req.params.id]);
+    
+    if(result.length == 0){
+        return res.status(404).json({message: "Producto no encontrado"});
+    }
+
+    res.json(result);
 }
 
-export const createTasks = (req, res) => {
-    const {title, description}= req.body;
-    pool.query('');
-    res.send('creando tareas')
+export const createTasks = async(req, res) => {
+    const {titulo, descripcion, precio}= req.body;
+    const [result] = await pool.query('INSERT INTO discos(titulo, descripcion, precio) VALUES (?, ?, ?) ',
+    [
+        titulo,
+        descripcion,
+        precio
+    ]);
+
+    console.log(result);
+    res.json({
+        id: result.insertId, titulo, descripcion, precio
+    });
 }
 
-export const updateTask = (req, res) => {
-    res.send('actualizando tarea')
+export const updateTask = async(req, res) => {
+    const [result] = await pool.query('UPDATE discos SET ? WHERE id = ?', [req.body, req.params.id]);
+
+    res.json(result);
 }
 
-export const deleteTasks = (req, res) => {
-    res.send('borrando tareas')
+export const deleteTasks = async(req, res) => {
+    const [result] = await pool.query('DELETE FROM discos WHERE id = ?', [req.params.id]);
+    
+    if(result.affectedRows == 0){
+        return res.status(404).json({message: "Producto no encontrado"});
+    }
+
+    return res.sendStatus(204);
 }
